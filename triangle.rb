d7d1cc32c031c0a5fa71aa1ac3a1723cc93a8550
Matt Ryan <m@mrevd.me>
2014-02-21 18:24:01 -0500

    Continued enlightenment

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,21 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sorted = [a, b, c].sort!
+
+  if sorted[0] + sorted[1] <= sorted[2]
+    raise TriangleError, "Two combined sides of a triangle must be greater than the third."
+  end
+
+  if sorted.min <= 0
+    raise TriangleError, "Each side must have a length greater than 0."
+  end
+
+  case [a, b, c].uniq.size
+  when 1 then :equilateral
+  when 2 then :isosceles
+  else        :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
